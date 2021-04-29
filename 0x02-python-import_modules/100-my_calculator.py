03a9184d4e3c3d617aeb59a859e409c33df2838b
Sandro <3212@holbertonschool.com>
2021-04-29 14:02:45 +0000

    Added if for each case

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,15 +7,17 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    
-    a = int(argv[1])
-    b = int(argv[3])
-
-    operators = { '+' : add, '-' : sub,'*' : mul, '/' : div }
-    operator = argv[2]
-
-    if not operator in operators:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-
-    print('{:d} {} {:d} = {:d}'.format(a, operator, b, operators[operator](a, b)))
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        elif argv[2] == '/':
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
