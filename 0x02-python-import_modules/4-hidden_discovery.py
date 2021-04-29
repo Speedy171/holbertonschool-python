7411d3b58291cbd8ba8aefb90353744898637acb
Sandro <3212@holbertonschool.com>
2021-04-29 13:25:52 +0000

    Replaced comparison with starts with

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def main():
     for name in dir(hidden_4):
-        if name[:2] != '__':
+        if not name.startswith('__'):
             print(name)
 if __name__ == '__main__':
     main()
