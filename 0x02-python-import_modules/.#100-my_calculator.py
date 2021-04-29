9d76fdd56d4fbfa9da2fd1619124580c2bdde355
Sandro <3212@holbertonschool.com>
2021-04-29 13:40:31 +0000

    Added 6th command

---
+++ 0x02-python-import_modules/.#100-my_calculator.py
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.2127:1619699787
---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    
+    a = int(argv[1])
+    b = int(argv[3])
+    operators = { '+' : add,
+                  '-' : sub,
+                  '*' : mul,
+                  '/' : div}
+    operator = argv[2]
+
+    if not operator in operators:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+    print('{:d} + {:d} = {:d}'.format(a, b, operators[operator](a, b)))
