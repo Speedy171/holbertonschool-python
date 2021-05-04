85b44a134c2384021b66566c69eee44b8874d49f
Sandro <3212@holbertonschool.com>
2021-05-04 20:17:57 +0000

    Replaced loop with map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        squares = []
-        for row in matrix:
-            rowsquared = []
-            for x in row:
-                rowsquared.append(x ** 2)
-            squares.append(rowsquared)
-    return squares
+    new_matrix = []
+
+    for x in matrix:
+        val = list(map(lambda mul: mul ** 2, x))
+        new_matrix.append(val)
+
+    return(new_matrix)
