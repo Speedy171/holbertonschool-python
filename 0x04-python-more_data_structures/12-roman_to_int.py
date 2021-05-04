d5904bccdba624c9175d7d1d0289db4acb6d541b
Sandro <3212@holbertonschool.com>
2021-05-04 20:26:23 +0000

    Fixed 12th command

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-   if (type(roman_string) != str) or roman_string is None:
+    if (type(roman_string) != str) or roman_string is None:
         return 0
-    romannumbers = {'M': 1000, 'D': 500, 'C': 100,
-            'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    prev, sum = 0, 0
-    for letter in roman_string:
-        sum += romannumbers[letter] if romannumbers[letter] <= prev else romannumbers[letter] - prev * 2
-        prev = romannumbers[letter]
-    return sum
-
+    count = 0
+    tmp = 0
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for iter1 in roman_string:
+        if tmp >= int(dict.get(iter1)) and tmp != 0:
+            count += int(dict.get(iter1))
+        elif tmp < int(dict.get(iter1)) and tmp != 0:
+            count = count + ((int(dict.get(iter1)) - tmp) - tmp)
+        elif tmp == 0:
+            count += int(dict.get(iter1))
+        tmp = int(dict.get(iter1))
+    return (count)
