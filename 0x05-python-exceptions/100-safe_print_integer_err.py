9b13e0df529671b091dd6c16582788cbf347eb6e
Sandro <3212@holbertonschool.com>
2021-05-06 16:12:17 +0000

    Updated 7th command

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception:", e, file=sys.stderr)
+        sys.stderr.write("Exception: {}".format(e))
         return False
