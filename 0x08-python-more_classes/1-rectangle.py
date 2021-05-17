f45eb42e2c87a46c4e8f0934a28765bb1f0e8252
Sandro <3212@holbertonschool.com>
2021-05-17 17:48:48 +0000

    removed _ from init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the class"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
