95ab0ee196f691b9a6a47799d47838aafddffcd6
Sandro <3212@holbertonschool.com>
2021-05-17 18:05:01 +0000

    Modified 5th command

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-     Module defining rectangle
+    Module defining rectangle
+    return {}
 """
 
 
-class Rectangle:
+class Rectangle():
     """rectangle class"""
 
     def __init__(self, width=0, height=0):
-        """Initialize"""
+        """Initialize class."""
         self.width = width
         self.height = height
 
@@ -17,20 +18,20 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the current Rectangle area."""
+        """Return Rectangle area"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
 
     @property
     def width(self):
-        """Retrieve width of Rectangle"""
+        """Retrieve width of the Rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set width of Rectangle"""
-        if type(value) not in [int]:
+        """Set width of the Rectangle"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -43,15 +44,15 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Set height of Rectangle"""
-        if type(value) not in [int]:
+        """Set height of theRectangle class"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def __str__(self):
-        """Print the rectangle using the char #"""
+        """Print the rectangle with the char #"""
         if self.__height == 0 or self.__width == 0:
             return("")
         else:
@@ -63,7 +64,7 @@ class Rectangle:
         return(str1[:-1])
 
     def __repr__(self):
-        """string evaluation of rectangle"""
+        """Get string evaluation of rectangle"""
         if self.__height == 0 or self.__height == 0:
             return("")
         else:
