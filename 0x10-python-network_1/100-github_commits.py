c7104c0a8ac981ad5fd48958064f8c8f0b01e326
Sandro <3212@holbertonschool.com>
2021-06-07 10:06:40 +0000

    Fixed github optional task

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -10,11 +10,16 @@ from sys import argv
 
 if __name__ == "__main__":
     github_commits = 'https://api.github.com/repos/'\
-    + argv[2] + '/' + argv[1] + '/commits'
-    data = {'per_page':10, 'page':1}
-    req = requests.get(github_commits, data)
-    res = req.json()
-
-    for r in res:
-        print(r.get('sha') + ': ', end="")
-        print(r.get('commit').get('author').get('name'))
+                     + argv[2] + '/' + argv[1] + '/commits'
+    req = requests.get(github_commits)
+    if "json" not in req.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        res = req.json()
+        i = 0
+        for r in res:
+            if i > 9:
+                break
+            print(r.get('sha') + ': ', end="")
+            print(r.get('commit').get('author').get('name'))
+            i += 1
