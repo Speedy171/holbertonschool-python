c48c9f51ee8a43df3b16f3e7e7691d97e4bfc5af
Speedy171 <3212@holbertonschool.com>
2021-07-09 20:09:25 +0000

    Modified prime game

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -9,31 +9,28 @@ def isWinner(x, nums):
         return None
     Maria_score = 0
     Ben_score = 0
-    max_num = max(nums) + 1
-    prime_number = [False] * max_num 
+    prime_number = [False] * 100001
     p = []
-    for i in range(2, max_num):
+    for i in range(2, 100001):
         if not prime_number[i]:
             p.append(i)
-            for j in range(i, max_num , i):
+            for j in range(i, 100001, i):
                 prime_number[j] = True
 
     l = 0
     for a0 in range(x):
         n = nums[l]
         num = 0
-        for x in range(max_num):
+        for x in range(100001):
             if x >= len(p) or p[x] > n:
                 break
             num += 1
         l += 1
-        if num % 2 == 1:
+        if (['Maria', 'Ben'][(num % 2) ^ 1]) == "Maria":
             Maria_score += 1
         else:
             Ben_score += 1
     if Maria_score > Ben_score:
         return "Maria"
-    else if Ben_score < Maria_score:
+    else:
         return "Ben"
-    else
-        return None
